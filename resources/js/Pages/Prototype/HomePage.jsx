be1ab8ec5cff1f3f74217db224894a83db3689da
BagasKelana <hilalbagas@gmail.com>
2023-12-16 00:03:09 +0700

    feat: add toggle dark mode [HomePage]

--- resources/js/Pages/Prototype/HomePage.jsx
+++ resources/js/Pages/Prototype/HomePage.jsx
@@ -1,11 +1,14 @@
 import { Head } from "@inertiajs/react";
+import ModeToggle from "@/Components/ModeToggle";
 
 export default function Home() {
     return (
         <>
             <Head title="Home" />
-
-            <h1>Home Page</h1>
+            <div class="flex flex-col w-full min-h-screen bg-white dark:bg-black dark:text-white">
+                <h1 className="text-3xl">halo</h1>
+                <ModeToggle />
+            </div>
         </>
     );
 }
