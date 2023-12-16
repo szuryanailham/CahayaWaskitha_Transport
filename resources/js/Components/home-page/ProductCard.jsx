3513a935f0578bc7a15f9cd056db6b88a84f22a9
BagasKelana <hilalbagas@gmail.com>
2023-12-17 04:11:11 +0700

    feat: responsive page [HomePage]

--- resources/js/Components/home-page/ProductCard.jsx
+++ resources/js/Components/home-page/ProductCard.jsx
@@ -7,14 +7,14 @@ const ProductCard = ({ className, name, src, price }) => {
     const harga = 100000;
     return (
         <div className={cn("inline-block w-full", className)}>
-            <div className=" relative min-w-full">
+            <div className="relative min-w-full ">
                 <div className="h-full">
-                    <Card className="max-w-[403px] min-h-[372px] w-full bg-white xl:bg-gray-100 dark:bg-gray-600 rounded-none shadow-card shadow relative">
-                        <CardContent className="flex w-full justify-between pt-9 xl:pt-0 xl:px-4 xl:absolute xl:left-0 xl:top-4  z-[2]  ">
+                    <Card className="max-w-[403px] min-h-[372px] w-full bg-transparent xl:bg-gray-100 dark:bg-gray-800/50 rounded-none shadow-card shadow relative">
+                        <CardContent className="flex w-full justify-between pt-4 xl:pt-0 xl:px-4 xl:absolute xl:left-0 xl:top-4 z-[2]">
                             <CardTitle className="font-bold">
                                 Toyota Innova
                             </CardTitle>
-                            <CardTitle className="break-words font-bold ">
+                            <CardTitle className="break-words font-bold">
                                 Rp
                                 {harga.toLocaleString("id-ID", {
                                     currency: "IDR",
@@ -30,47 +30,47 @@ const ProductCard = ({ className, name, src, price }) => {
                                     loading="lazy"
                                 />
                             </div>
-                            <div className="w-full flex items-center justify-center space-x-14 xl:absolute xl:-bottom-4 xl:left-0 font-semibold">
+                            <div className="w-full flex items-center justify-center space-x-5 md:space-x-10 xl:space-x-14 mt-4 xl:absolute xl:-bottom-4 xl:left-0 font-semibold">
                                 <figure className="items-center flex flex-col ">
                                     <img
-                                        className="w-[25px] h-[25px] dark:invert"
+                                        className="h-5 w-5 md:w-[25px] md:h-[25px] dark:invert"
                                         height={25}
                                         width={25}
                                         src="/🦆 icon _clock_.svg"
                                         alt="icon.svg"
                                     />
-                                    <figcaption className="whitespace-nowrap">
+                                    <figcaption className="whitespace-nowrap max-lg:text-[10pt]">
                                         12 Jam
                                     </figcaption>
                                 </figure>
-                                <figure className="items-center flex flex-col  ">
+                                <figure className="items-center flex flex-col">
                                     <img
-                                        className="w-[25px] h-[25px] dark:invert"
+                                        className="h-5 w-5 md:w-[25px] md:h-[25px]  dark:invert"
                                         height={25}
                                         width={25}
                                         src="/User.svg"
                                         alt="icon.svg"
                                     />
-                                    <figcaption className="whitespace-nowrap">
+                                    <figcaption className="whitespace-nowrap max-lg:text-[10pt]">
                                         6 Orang
                                     </figcaption>
                                 </figure>
-                                <figure className="items-center flex flex-col  ">
+                                <figure className="items-center flex flex-col">
                                     <img
-                                        className="w-[25px] h-[25px] dark:invert"
+                                        className="h-5 w-5 md:w-[25px] md:h-[25px] dark:invert"
                                         height={25}
                                         width={25}
                                         src="/Gas station.svg"
                                         alt="icon.svg"
                                     />
-                                    <figcaption className="whitespace-nowrap">
+                                    <figcaption className="whitespace-nowrap max-lg:text-[10pt]">
                                         Full Bensin
                                     </figcaption>
                                 </figure>
                             </div>
                         </CardContent>
                         <CardFooter className="p-6">
-                            <div className="w-full flex justify-evenly mt-12 text-center">
+                            <div className="w-full flex justify-evenly mt-4 xl:mt-12 text-center">
                                 <Button
                                     size="lg"
                                     className="rounded-none text-lg font-bold"
--- resources/js/Pages/Prototype/HomePage.jsx
+++ resources/js/Pages/Prototype/HomePage.jsx
@@ -23,42 +23,59 @@ export default function Home({ categories, units }) {
         <>
             <Head title="Home" />
 
-            <main className="w-full h-full bg-white dark:bg-gray-900">
+            <main className="w-full h-full bg-white dark:bg-gray-800">
                 <ModeToggle />
-                <section className="w-full" id="hero-section">
+                <section className="w-full px-3" id="hero-section">
                     <div className="w-full h-full">
                         <div className="w-full h-full flex justify-center">
                             <img
-                                className="object-cover h-[439px] w-[1171px] object-top rounded-2xl "
+                                className="object-cover min-h-[124px] h-[150px] md:h-[330px] xl:h-[439px] max-w-[1171px] w-full object-top rounded-2xl "
                                 src="/2880x1800-px-assassins-creed-Assassins-Creed-Black-Flag-video-games-1062323-wallhere.com (1) (1) (1).jpg"
                                 alt=""
                             />
                         </div>
                     </div>
                 </section>
+                <section className="w-full mt-2 md:mt-0 xl:hidden">
+                    <div className="w-full px-6 flex md:hidden">
+                        <h2 className="text-[12pt] font-bold">
+                            Pencarian Terbaru
+                        </h2>
+                    </div>
+                    <div className="flex w-full items-center justify-center mt-2 gap-6">
+                        <Button className="rounded-2xl py-3 px-3 h-5 text-[10pt] font-light">
+                            Mobil Murah
+                        </Button>
+                        <Button className="rounded-2xl py-3 px-3 h-5 text-[10pt] font-light">
+                            Mobil Murah
+                        </Button>
+                        <Button className="rounded-2xl py-3 px-3 h-5 text-[10pt] font-light">
+                            Mobil Murah
+                        </Button>
+                    </div>
+                </section>
                 <section
-                    className="w-full h-full py-[50px] px-4"
+                    className="w-full h-full py-[30px] xl:py-[50px] px-2 xl:px-4"
                     id="products-section"
                 >
-                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-12 place-items-stretch justify-items-center">
+                    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-4 lg:gap-y-12 place-items-stretch justify-items-center">
                         {renderProduct()}
                     </div>
                 </section>
-                <footer className="w-full flex flex-col items-center ">
-                    <div className="w-[403px]">
-                        <h1 className="font-bold text-[36pt] leading-[150%] text-center">
-                            Cahaya Waskitha Transport
-                        </h1>
-                    </div>
-                    <div className="flex space-x-20 px-36 py-10">
-                        <Instagram className="w-[30px] h-[30px]" />
-                        <Youtube className="w-[30px] h-[30px]" />
-                        <Facebook className="w-[30px] h-[30px]" />
-                        <Facebook className="w-[30px] h-[30px]" />
-                    </div>
-                    <Button />
-                </footer>
             </main>
+            <footer className="w-full flex flex-col items-center ">
+                <div className="md:w-[403px] w-[224px]">
+                    <h1 className="font-bold text-[20pt] md:text-[30pt] xl:text-[36pt]  leading-[150%] text-center">
+                        Cahaya Waskitha Transport
+                    </h1>
+                </div>
+                <div className="flex w-full justify-center space-x-10 md:space-x-16 xl:space-x-20 xl:px-36 py-10">
+                    <Instagram className="w-[30px] h-[30px]" />
+                    <Youtube className="w-[30px] h-[30px]" />
+                    <Facebook className="w-[30px] h-[30px]" />
+                    <Facebook className="w-[30px] h-[30px]" />
+                </div>
+            </footer>
         </>
     );
 }
