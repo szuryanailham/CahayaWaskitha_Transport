6043b9e7f09d6872aa48fc3d3177bb9f237f8520
BagasKelana <hilalbagas@gmail.com>
2024-01-08 06:56:58 +0700

    refactor: navmenu

--- resources/js/Components/home-page/NavMenu.jsx
+++ resources/js/Components/home-page/NavMenu.jsx
@@ -15,51 +15,13 @@ import {
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 
-const components = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-];
-
 export function NavigationMenuDemo({ categories, units }) {
     console.log(categories);
     return (
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger className="font-bold">
+                    <NavigationMenuTrigger className="font-bold text-xs md:text-base">
                         Kategori
                     </NavigationMenuTrigger>
                     <NavigationMenuContent>
@@ -98,7 +60,7 @@ export function NavigationMenuDemo({ categories, units }) {
                     </NavigationMenuContent>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger className="font-bold">
+                    <NavigationMenuTrigger className="font-bold text-xs md:text-base">
                         List Kendaraan
                     </NavigationMenuTrigger>
                     <NavigationMenuContent>
@@ -118,7 +80,7 @@ export function NavigationMenuDemo({ categories, units }) {
                 <NavigationMenuItem>
                     <Link as="button" href="/">
                         <NavigationMenuLink
-                            className={navigationMenuTriggerStyle()}
+                            className={`${navigationMenuTriggerStyle()}text-xs md:text-base`}
                         >
                             Semua Kendaraan
                         </NavigationMenuLink>
