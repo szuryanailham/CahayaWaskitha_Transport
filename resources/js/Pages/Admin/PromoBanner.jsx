a51f921f5485a7568b16379b34c56267496eae71
titik444 <35260719+titik444@users.noreply.github.com>
2024-01-23 21:17:57 +0700

    feat: implement promo banner functionality with model, controller, and views

---
+++ resources/js/Pages/Admin/PromoBanner.jsx
@@ -0,0 +1,56 @@
+import { Head, useForm } from "@inertiajs/react";
+
+export default function PromoBanner({ banners }) {
+    console.log(banners);
+
+    const { data, setData, post, processing, reset, errors } = useForm({
+        title: "",
+        image: "",
+    });
+
+    const onHandleChange = (event) => {
+        setData(
+            event.target.name,
+            event.target.type === "file"
+                ? event.target.files[0]
+                : event.target.value
+        );
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post("/admin/promo-banner", {
+            preserveScroll: true,
+            onSuccess: () => reset("title", "image"),
+        });
+    };
+
+    return (
+        <>
+            <Head title="Promo Banner" />
+
+            <form onSubmit={submit}>
+                <input
+                    type="text"
+                    name="title"
+                    value={data.title}
+                    className={`${
+                        errors.name ? "is-invalid" : ""
+                    } border border-gray-200 p-2 w-full mb-3 dark:bg-gray-800`}
+                    onChange={onHandleChange}
+                />
+                <br />
+                <input type="file" name="image" key={data.image} onChange={onHandleChange} />
+                <br />
+                <button
+                    type="submit"
+                    disabled={processing}
+                    className="w-full py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    {processing ? "Loading..." : "Submit"}
+                </button>
+            </form>
+        </>
+    );
+}
