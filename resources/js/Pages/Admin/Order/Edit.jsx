e3909349682a47e3898b1c5ec786a6307f353cf9
titik444 <35260719+titik444@users.noreply.github.com>
2024-01-25 12:47:55 +0700

    fix: adjust casting for 'start_time' and 'end_time' in model for accurate datetime handling

--- resources/js/Pages/Admin/Order/Edit.jsx
+++ resources/js/Pages/Admin/Order/Edit.jsx
@@ -1,4 +1,4 @@
-import { Head, Link, router, useForm } from "@inertiajs/react";
+import { Head, Link, router, useForm, usePage } from "@inertiajs/react";
 import { useEffect, useState } from "react";
 
 export default function Dashboard({ order, units, unit, time }) {
