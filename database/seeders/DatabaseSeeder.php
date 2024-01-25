4e4fdf5ae0b0e9d6fc4de06458679381d872b120
titik444 <35260719+titik444@users.noreply.github.com>
2024-01-25 17:06:03 +0700

    feat: add TestimonyTableSeeder for seeding testimonial data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             UserTableSeeder::class,
             UnitTableSeeder::class,
             UnitImageTableSeeder::class,
+            TestimonyTableSeeder::class,
         ]);
     }
 }
